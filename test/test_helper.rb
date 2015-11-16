c6e349633cfaa81b9d14fde2b62e3bce85c4817f
Mark Morga <mmorga@rackspace.com>
2015-11-16 17:00:40 -0600

    Added minimum coverage, moved coverage to tmp/coverage, ignored vendor source

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,9 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  minimum_coverage 88
+  coverage_dir 'tmp/coverage'
+  add_filter "/vendor\/raml_ruby/"
+end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ramlstyle'
